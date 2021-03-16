a22af5f78629f1003b8fe4b19c249e4268f5a86f
m3-welch <morganwelch@hotmail.com>
2021-03-16 10:18:27 +0000

    Task 1 & 2 complete, task 3 underway

---
+++ task_3/README.md
@@ -0,0 +1,24 @@
+# Advanced Topics in Web Development 2 - Task 3
+## Task 3: Charts Visualisation (20 marks)
+### Use [Google Chart](https://developers.google.com/chart/) or any other charts API to visualise the following charts:
+
+A scatter chart to show a years worth of data (averaged by month) from a specific station for Carbon Monoxide (NO) at a certain time of day - say 08.00 hours.
+
+A line chart showing levels in any 24 hour period on any day (user selectable) for any of the six stations (user selectable) for any of the major pollutants (nox, no, no2) in the date range downloaded.
+
+All charts should give an accurate rendition of the data set at an appropriate resolution.
+
+Note that you can restrict the data to the range 01/01/2015 to 31/12/2019
+
+## Colour Palette
+### Found on [Coolers.co](https://coolors.co/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529)
+
+`#F8F9FA` Cultured
+`#E9ECEF` Cultured
+`#DEE2E6` Gainsboro
+`#CED4DA` Light Gray
+`#ADB5BD` Cadet Blue Crayola
+`#6C757D` Slate Gray
+`#495057` Davys Grey
+`#343A40` Gunmetal
+`#212529` Charleston Green
---
+++ task_3/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ATIWD2 - Task 3</title>
+        <link rel="stylesheet" href="stylesheet.css"/>
+        <link href="https://unpkg.com/mono-icons@1.2.4/iconfont/icons.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div class="top-menu-bar">
+            <div class="top-menu-bar-item">
+                <span class="top-menu-bar-title">ATIWD2 - Task 3</span>
+            </div>
+
+            <div class="top-menu-bar-item">
+                <div class="top-menu-bar-button" onclick="coCertainTimeOfDay()">
+                    <i class="mi-clock"></i>
+                    View Nitrogen Monoxide Levels for a Certain Time of Day
+                </div>
+            </div>
+
+            <div class="top-menu-bar-item">
+                <div class="top-menu-bar-button" onclick="allLevels6Stations()">
+                    <i class="mi-calendar"></i>
+                    View All Levels for a Certain Day From 6 Stations
+                </div>
+            </div>
+
+            <div class="top-menu-bar-item me">
+                <span class="top-menu-bar-text"><i class="mi-user"></i>Morgan Welch - 17006647</span>
+            </div>
+        </div>
+    </body>
+</html>
